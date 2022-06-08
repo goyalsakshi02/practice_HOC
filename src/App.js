b61af5b594716fab9eda6d7fe67389d52b6d0a3f
Sakshi Goyal <sakshi.goyal@dianapps.com>
2022-06-08 10:24:27 +0530

    update

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function HOCBlue(props)
 function Counter()
 {
   const [count,setCount]=useState(0)
-  return<div>
+  return <div>
     <h3>{count}</h3>
     <button onClick={()=>setCount(count+1)}>Update</button>
   </div>
