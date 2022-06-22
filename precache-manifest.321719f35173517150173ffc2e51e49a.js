dff25b30d6395d849a9b1ed7076b6fb9381f8d41
Sakshi Goyal <sakshi.goyal@dianapps.com>
2022-06-22 16:16:04 +0530

    Updates

---
+++ precache-manifest.321719f35173517150173ffc2e51e49a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f0dad29aac29be48fd9b",
+    "url": "/practice_HOC/static/css/main.90ea15a2.chunk.css"
+  },
+  {
+    "revision": "f0dad29aac29be48fd9b",
+    "url": "/practice_HOC/static/js/main.f0dad29a.chunk.js"
+  },
+  {
+    "revision": "af08dc77e4e76da41e54",
+    "url": "/practice_HOC/static/js/1.af08dc77.chunk.js"
+  },
+  {
+    "revision": "6ae2d67e4d756e5cf808",
+    "url": "/practice_HOC/static/js/2.6ae2d67e.chunk.js"
+  },
+  {
+    "revision": "7d9b3a36d7f623e8dee4",
+    "url": "/practice_HOC/static/js/runtime~main.7d9b3a36.js"
+  },
+  {
+    "revision": "9248954d9a064f4ec5ede2448e037f45",
+    "url": "/practice_HOC/index.html"
+  }
+];
